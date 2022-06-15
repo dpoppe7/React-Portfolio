5f02ac46cdf8055760376c35fc3b97167e9c9c6a
dpoppe7 <70035439+dpoppe7@users.noreply.github.com>
2022-06-14 23:24:53 -0600

    Added header section , first title and buttons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const Header = () => {
   return (
-    <div>header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Damaris</h1>
+        <h5 className="text-light">Full-stack developer</h5>
+        <CTA/>
+      </div>
+    </header>
   )
 }
 
