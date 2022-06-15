054bcacc993e9c5ee87144f2c48583afb348c968
dpoppe7 <70035439+dpoppe7@users.noreply.github.com>
2022-06-14 23:57:31 -0600

    imported react-icons and  initial icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import ME from '../../assets/Me.jpg'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -10,6 +12,13 @@ const Header = () => {
         <h1>Damaris</h1>
         <h5 className="text-light">Full-stack developer</h5>
         <CTA/>
+        <HeaderSocials/> 
+
+        <div className="me">
+          <img src={ME} alt="" ></img>
+        </div>
+
+        <a href="#contact" classname='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )
