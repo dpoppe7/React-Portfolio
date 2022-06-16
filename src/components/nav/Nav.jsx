97c5ae261d188ef052d61ab539f9b872cdb557e1
dpoppe7 <70035439+dpoppe7@users.noreply.github.com>
2022-06-16 13:16:30 -0600

    added home bar experience icon

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
 import {BiUser} from 'react-icons/bi'
+import {BiBook} from 'react-icons/bi'
 
 const Nav = () => {
   return (
     <nav>
     <a href="#"><AiOutlineHome/></a>
     <a href="#about"><BiUser/></a>
+    <a href="#experience"><BiBook/></a>
     </nav>
   )
 }
