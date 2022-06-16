4995b16ccc8a74864ae7c44c4d4a796db4a0618d
dpoppe7 <70035439+dpoppe7@users.noreply.github.com>
2022-06-16 13:17:26 -0600

    added home bar services and contact icon

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,13 +3,17 @@ import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
 import {BiUser} from 'react-icons/bi'
 import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {TbMessage} from 'react-icons/tb'
 
 const Nav = () => {
   return (
     <nav>
-    <a href="#"><AiOutlineHome/></a>
-    <a href="#about"><BiUser/></a>
-    <a href="#experience"><BiBook/></a>
+      <a href="#"><AiOutlineHome/></a>
+      <a href="#about"><BiUser/></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#services"><RiServiceLine/></a>
+      <a href="#contact"><TbMessage/></a>
     </nav>
   )
 }
