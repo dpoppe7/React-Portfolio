6011f8d277cee04e45d5aad20e5cd047c96020fb
dpoppe7 <70035439+dpoppe7@users.noreply.github.com>
2022-06-16 13:14:25 -0600

    added home bar about icon

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
+import {BiUser} from 'react-icons/bi'
 
 const Nav = () => {
   return (
     <nav>
     <a href="#"><AiOutlineHome/></a>
+    <a href="#about"><BiUser/></a>
     </nav>
   )
 }
