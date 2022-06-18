b86cc5f268eaac57397a89ff5c860d3fdb7721a9
dpoppe7 <70035439+dpoppe7@users.noreply.github.com>
2022-06-17 23:29:03 -0600

    added html for about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me2.jpeg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {AiOutlineFolderOpen} from 'react-icons/ai'
 
 const About = () => {
   return (
-    <section id='about'>About T</section>
+    <section id='about'>
+      <h5>Get to know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about_container'>
+        <div className='about__me'>
+          <img src={ME} alt="About Image"/>
+        </div>
+
+        <div className='about__content'>
+          <div className="about__cards">
+            
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3+ years working experience</small>
+            
+            </article>
+            <article cl assName='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>300+ clients.</small>
+            </article>
+            
+            <article className='about__card'>
+              <AiOutlineFolderOpen className='about__icon'/>
+              <h5>Projects</h5>
+              <small>80+ completed projects</small>
+            </article>
+
+            <p>
+              Here is some information about me and the different projects 
+              I've been working on as a computer science student.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
