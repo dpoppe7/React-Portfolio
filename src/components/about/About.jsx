69540950401fb9e1f0842b7e549541cd3bfbe0dc
dpoppe7 <70035439+dpoppe7@users.noreply.github.com>
2022-06-17 22:37:58 -0600

    completed floating navbar with setState and active State useState

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,7 @@ import './about.css'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>About T</section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,15 +5,17 @@ import {BiUser} from 'react-icons/bi'
 import {BiBook} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
 import {TbMessage} from 'react-icons/tb'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#"><AiOutlineHome/></a>
-      <a href="#about"><BiUser/></a>
-      <a href="#experience"><BiBook/></a>
-      <a href="#services"><RiServiceLine/></a>
-      <a href="#contact"><TbMessage/></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUser/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contacts' ? 'active' : ''}><TbMessage/></a>
     </nav>
   )
 }
