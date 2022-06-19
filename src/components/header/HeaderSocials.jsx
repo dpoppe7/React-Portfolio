f718b7bbba0cbd19b692c568aa72b34cd7411700
dpoppe7 <70035439+dpoppe7@users.noreply.github.com>
2022-06-19 17:45:35 -0600

    completed portfolio template

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -8,7 +8,7 @@ const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"><FaGithub/></a>
+        <a href="https://github.com/dpoppe7" target="_blank"><FaGithub/></a>
         <a href="https://dribble.com" target="_blank"><FiDribbble/></a>
         <a href="https://instagram.com" target="_blank"><FiInstagram/></a>
     </div>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/Sky orange.jpeg'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+              <img src={IMG1} alt=""/>
+          </div>
+          <h3>This is a portfolio item Title</h3>
+          <div className='portfolio__item-cla'>
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+              <img src={IMG1} alt=""/>
+          </div>
+          <h3>This is a portfolio item Title</h3>
+          <div className='portfolio__item-cla'>
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+              <img src={IMG1} alt=""/>
+          </div>
+          <h3>This is a portfolio item Title</h3>
+          <div className='portfolio__item-cla'>
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+
+      </div>
+    </section>
   )
 }
 
