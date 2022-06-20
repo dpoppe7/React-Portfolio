16d4607b69205aeed0b07a210fba55d1f5f4f3f7
dpoppe7 <70035439+dpoppe7@users.noreply.github.com>
2022-06-20 00:22:19 -0600

    footer jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import './footer.css'
+import { FaFacebookF } from 'react-icons/fa'
+import { SiInstagram } from 'react-icons/si'
+import { AiOutlineTwitter } from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#' className='footer__logo'>Damaris</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#services'>Services</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#testimonials'>Testimonials</a></li>
+        <li><a href='#contacts'>Contacts</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href='https://facebook.com'><FaFacebookF/></a>
+        <a href='https://instagram.com'><SiInstagram/></a>
+        <a href='https://twitter.com'><AiOutlineTwitter/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Damaris Poppe. All right reserved.</small>
+      </div>
+
+    </footer>
   )
 }
 
