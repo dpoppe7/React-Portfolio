fe22c7a9239a61560d0969876c7c29eda868b80c
dpoppe7 <70035439+dpoppe7@users.noreply.github.com>
2022-06-19 22:02:30 -0600

    Finished portfolio + added testimonials jx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,31 @@ import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/Sky orange.jpeg'
 
+/* Array of Data */
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Crypto currency dashboard & financial visualization',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/'
+  },
+  {
+    id: 2,
+    image: IMG1,
+    title: 'Crypto currency dashboard & financial visualization',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/'
+  },
+  {
+    id: 3,
+    image: IMG1,
+    title: 'Crypto currency dashboard & financial visualization',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/'
+  }
+]
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
@@ -9,39 +34,22 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-              <img src={IMG1} alt=""/>
-          </div>
-          <h3>This is a portfolio item Title</h3>
-          <div className='portfolio__item-cla'>
-            <a href="https://github.com" className='btn' target='_blank'>Github</a>
-            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-              <img src={IMG1} alt=""/>
-          </div>
-          <h3>This is a portfolio item Title</h3>
-          <div className='portfolio__item-cla'>
-            <a href="https://github.com" className='btn' target='_blank'>Github</a>
-            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-              <img src={IMG1} alt=""/>
-          </div>
-          <h3>This is a portfolio item Title</h3>
-          <div className='portfolio__item-cla'>
-            <a href="https://github.com" className='btn' target='_blank'>Github</a>
-            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title}/>
+              </div>
+              <h3>{title}</h3>
+              <div className='portfolio__item-cla'>
+                <a href={github} className='btn' target='_blank'>Github</a>
+                <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+              </div>
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import './testimonials.css'
+import AVT1 from '../../assets/LAview.jpg'
 
 const Testimonials = () => {
   return (
-    <section id='testimonial'>Testimonials</section>
+    <section id='testimonial'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVT1} alt="Avatar"/>
+            <h5 className='client__name'>Achievement</h5>
+            <small className='client__review'>
+              Here you should insert a review from a client.
+            </small>
+          </div>
+        </article>
+
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVT1} alt="Avatar"/>
+            <h5 className='client__name'>Achievement</h5>
+            <small className='client__review'>
+              Here you should insert a review from a client.
+            </small>
+          </div>
+        </article>
+
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVT1} alt="Avatar"/>
+            <h5 className='client__name'>Achievement</h5>
+            <small className='client__review'>
+              Here you should insert a review from a client.
+            </small>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
