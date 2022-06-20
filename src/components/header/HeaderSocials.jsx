6bac34ef7460698f9e7c6b0967088f8325b75161
dpoppe7 <70035439+dpoppe7@users.noreply.github.com>
2022-06-19 19:32:13 -0600

    Added and updated social link

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
-import {FiDribbble} from 'react-icons/fi'
+import {SiFigma} from 'react-icons/si'
 import {FiInstagram} from 'react-icons/fi'
+import {SiVsco} from 'react-icons/si'
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
         <a href="https://github.com/dpoppe7" target="_blank"><FaGithub/></a>
-        <a href="https://dribble.com" target="_blank"><FiDribbble/></a>
-        <a href="https://instagram.com" target="_blank"><FiInstagram/></a>
+        <a href="https://figma.com/@dpoppe7" target="_blank"><SiFigma/></a>
+        <a href="https://instagram.com/dpoppe07" target="_blank"><FiInstagram/></a>
+        <a href="https://instagram.com/dpoppe07" target="_blank"><SiVsco/></a>
     </div>
   )
 }
