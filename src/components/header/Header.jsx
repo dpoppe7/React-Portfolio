51961850d6640917a441fdeaa74d57bbf3de3369
dpoppe7 <70035439+dpoppe7@users.noreply.github.com>
2022-06-21 13:35:23 -0600

    Changed header description

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Damaris Poppe</h1>
-        <h5 className="text-light">Full-stack developer</h5>
+        <h5 className="text-light">I'm a Computer Science Student working towards building creative tech solutions. </h5>
         <CTA/>
         <HeaderSocials/> 
 
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
-import {SiFigma} from 'react-icons/si'
+import {FiLinkedin} from 'react-icons/fi'
+import {FiGithub} from 'react-icons/fi'
+import {FiFigma} from 'react-icons/fi'
 import {FiInstagram} from 'react-icons/fi'
 import {SiVsco} from 'react-icons/si'
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com/dpoppe7" target="_blank"><FaGithub/></a>
-        <a href="https://figma.com/@dpoppe7" target="_blank"><SiFigma/></a>
+        <a href="https://linkedin.com" target="_blank"><FiLinkedin/></a>
+        <a href="https://github.com/dpoppe7" target="_blank"><FiGithub/></a>
+        <a href="https://figma.com/@dpoppe7" target="_blank"><FiFigma/></a>
         <a href="https://instagram.com/dpoppe07" target="_blank"><FiInstagram/></a>
         <a href="https://vsco.co/damariscph/gallery" target="_blank"><SiVsco/></a>
     </div>
