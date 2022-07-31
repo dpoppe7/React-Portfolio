f16582faad8a2f4e2ab7a28778a20c00b78fde2d
dpoppe7 <70035439+dpoppe7@users.noreply.github.com>
2022-07-30 22:04:19 -0600

    initial changes for portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import './portfolio.css'
+import {FiGithub} from 'react-icons/fi'
 import IMG1 from '../../assets/Sky orange.jpeg'
+import P1_pic1 from '../../assets/P1_pic1.png'
+import P1_pics from '../../assets/P1_pics.png'
 
 /* Array of Data */
 const data = [
   {
     id: 1,
-    image: IMG1,
+    image: P1_pics,
     project: 'Web Dev Project',
-    title: 'Social Network',
+    title: 'Social Network - MyfaceSpace',
     github: 'https://github.com/dpoppe7/webDev-Social-Network-Project',
+    description: 'A Social Networking website that works similar to the first version of Facebook. The scripts interact with a database where data for different accounts is stored. This project implements user authentication, sessions and a simple clean UI.',
+    tags: 'Database HTML CSS Javascript PHP MySQL',
     demo: 'http://taz.harding.edu/~dpoppe/myfacespace/login.php'
   },
   {
@@ -34,22 +39,25 @@ const Portfolio = () => {
   return (
     <section id='portfolio'>
       <h5>My Recent Work</h5>
-      <h2>Portfolio</h2>
+      <h2>Projects</h2>
 
       <div className="container portfolio__container">
         {
-          data.map(({id, image, project, title, github, demo}) => {
+          data.map(({id, image, project, title, github, description, tags, demo}) => {
             return (
               <article key={id} className="portfolio__item">
               <div className="portfolio__item-image">
                 <img src={image} alt={title}/>
               </div>
+
               <h5 className='project'>{project}</h5>
               <h3>{title}</h3>
-              <div className='portfolio__item-cla'>
-                <a href={github} className='btn' target='_blank'>Github</a>
-                <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
-              </div>
+
+              
+              <p className='description'>{description}</p>
+              
+              <a href="https://github.com/dpoppe7" target="_blank" className='project-link'><FiGithub/></a>
+              <p className='tags'>{tags}</p>
               </article>
             )
           })
