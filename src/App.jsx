407d59d84d1df5192ff471c1a08cddcf9c62304c
dpoppe7 <70035439+dpoppe7@users.noreply.github.com>
2022-08-02 00:50:39 -0600

    removed testimonials sec

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Fun from './components/fun/Fun'
 import Portfolio from './components/portfolio/Portfolio'
-import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
